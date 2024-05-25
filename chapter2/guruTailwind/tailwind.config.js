5b56f429d76b4bf7d7b5258daff0dd8ba02034f7
unknown <khaledsaoud237@gmail.com>
2024-05-25 14:01:00 +0100

    tailwind css

---
+++ chapter2/guruTailwind/tailwind.config.js
@@ -0,0 +1,50 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#d87d4a",
+        secondary: "#101010",
+        lightgray: "#f1f1f1",
+        softwhite: "#fafafa",
+        lightpink: "#fbaf85",
+      },
+      fontSize: {
+        title: "56px",
+        subtitle: "40px",
+        minititle: "32px",
+        text: "15px",
+      },
+      lineHeight: {
+        title: "58px",
+        subtitle: "44px",
+        minititle: "36px",
+        text: "25px",
+      },
+      letterSpacing: {
+        title: "2px",
+        subtitle: "1.5px",
+        minititle: "1.15px",
+      },
+      screens: {
+        tablette: "850px",
+      },
+      height: {
+        v1: "10vh",
+        v2: "20vh",
+        v3: "30vh",
+        v4: "40vh",
+        v5: "50vh",
+        v6: "60vh",
+        v7: "70vh",
+        v8: "80vh",
+        v9: "90vh",
+      },
+      backgroundImage: {
+        hero: "url(assets/home/desktop/image-hero.jpg)",
+      },
+      backgroundPosition: {
+        midcenter: "80% 80%",
+      },
+    },
+  },
+};
